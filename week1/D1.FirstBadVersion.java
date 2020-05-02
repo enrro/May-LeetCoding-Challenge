5b6ffd0c53cf55d79fd38fd239c01e7dfd558717
Edgar Alfredo Briceño Gonzalez <enrro@Edgars-MacBook-Pro.local>
2020-05-01 19:20:41 -0500

    first day

---
+++ week1/D1.FirstBadVersion.java
@@ -0,0 +1,63 @@
+/*
+You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+Example:
+
+Given n = 5, and version = 4 is the first bad version.
+
+call isBadVersion(3) -> false
+call isBadVersion(5) -> true
+call isBadVersion(4) -> true
+
+Then 4 is the first bad version. 
+*/
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        //System.out.println(n);
+        //System.out.println(binarySearch(0, n));
+        return binarySearch(0, n);
+    }
+    
+    public int binarySearch(int left, int right){
+        if(left > right){return -1;}
+        int middle = (left + right) /2;
+        if(isBadVersion(middle) && middle > 0 && !isBadVersion(middle-1) || 
+          isBadVersion(middle) && middle == 0){
+            return middle;
+        }
+        if(!isBadVersion(left) && isBadVersion(middle)){
+            return binarySearch(left,middle-1);
+        }else{
+            return binarySearch(middle+1, right);
+        }
+    }
+}
+
+
+//iterative solution.
+//left +rigth / 2 could overflow so we do it this way
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        int middle;
+        while(left < right){
+            middle = left + (right - left) / 2;
+            if(isBadVersion(middle)){
+                right = middle;
+            }else{
+                left = middle + 1;
+            }
+            
+        }
+        
+        return left;
+    }
+}
